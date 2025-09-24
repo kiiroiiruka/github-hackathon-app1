d00021b79c4b1e487fe633cdfdddf1034558b6d8
LAPTOP-1L70EI88\o_tsubasa <tubasan2005@gmail.com>
2025-09-24 12:08:20 +0900

    t

--- src/pages/dashboard/friends/index.jsx
+++ src/pages/dashboard/friends/index.jsx
@@ -1,13 +1,24 @@
 // src/pages/dashboard/friends/index.jsx
-import HeaderComponent from "../../../components/header/header"; // コンポーネントをimport
+import HeaderComponent2 from "../../../components/header/header2"; // コンポーネントをimport
 
 const FriendsAddScreen = () => {
   return (
     <div>
-      <HeaderComponent title="友達追加" />
-      <p>友達追加ページの内容</p>
+      <HeaderComponent2 title="友達追加" />
+      <div style={styles.container}>
+        <p>このユーザーIDを友達になりたい相手に送ろう。</p>
+      </div>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    display: "flex",           // flexbox を有効化
+    alignItems: "center",      // 縦方向に中央寄せ
+    height: "100vh",           // 画面全体の高さを確保
+    flexDirection: "column",   // 縦方向に配置（必要に応じて）
+  },
 };
 
 export default FriendsAddScreen;
