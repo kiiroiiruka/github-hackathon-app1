f9afbe0acf519f86d0c6018f94a2a0019644e8a8
kiiroiiruka <127270769+kiiroiiruka@users.noreply.github.com>
2025-09-27 16:09:28 +0900

    fix:firebase系のAPIキーのimport修正

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
-	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+	apiKey: "AIzaSyAeAU8XmgdiBK-J6zDTIBYlOFSBSLAvSZQ",
+	authDomain: "github-hackathon-app1.firebaseapp.com",
+	databaseURL: "https://github-hackathon-app1-default-rtdb.firebaseio.com/",
+	projectId: "github-hackathon-app1",
+	storageBucket: "github-hackathon-app1.firebasestorage.app",
+	messagingSenderId: "385402311621",
+	appId: "1:385402311621:web:44a0828f4b4619d11071ac",
+	measurementId: "G-QN6SJ1D6T7",
 };
 
 const app = initializeApp(firebaseConfig);
