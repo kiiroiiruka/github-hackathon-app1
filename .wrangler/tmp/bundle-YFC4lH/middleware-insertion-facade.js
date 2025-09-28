e96dd3c4c2bfa854c2b1179d4f3c2941461e6de6
kiiroiiruka <127270769+kiiroiiruka@users.noreply.github.com>
2025-09-29 02:11:45 +0900

    コミット

---
+++ .wrangler/tmp/bundle-YFC4lH/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\kiiro\\Github\\github-hackathon-app1\\.wrangler\\tmp\\pages-tFMXuO\\functionsWorker-0.7765205178539853.mjs";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\kiiro\\AppData\\Local\\nvm\\v22.14.0\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\Users\\kiiro\\AppData\\Local\\nvm\\v22.14.0\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\Users\\kiiro\\Github\\github-hackathon-app1\\.wrangler\\tmp\\pages-tFMXuO\\functionsWorker-0.7765205178539853.mjs";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
